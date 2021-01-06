3d1dbc75cf3f18d4c3c5a96a2a93de015e9ed78c
Eric M Shen <eric.mm.shen@gmai.com>
2021-01-06 11:35:48 -0500

    setup morning session stuff

--- personal-assistant/src/App.js
+++ personal-assistant/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import ExampleComponent from './ExampleComponent'
 
 const App = () => (
   <div>
     Beep boop hi there, I&apos;m your personal assistant hehe
+    <ExampleComponent alertText='you pressed me'></ExampleComponent>
   </div>
 )
 
